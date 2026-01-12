56930e1aca7e525e0ba588f0a5fb1afe22ee79ab
wuhanstudio <wuhanstudios@gmail.com>
2026-01-12 17:02:02 +0000

    [PSRAM] Add ESP32-S3 R8 support

--- main/sdcard.c
+++ main/sdcard.c
@@ -3,6 +3,9 @@
 #include <dirent.h>
 #include <esp_vfs_fat.h>
 
+#include <sdmmc_cmd.h>
+#include <driver/sdmmc_host.h>
+
 static const char *TAG = "sdcard";
 
 void list_recursive(const char *path) {
@@ -43,6 +46,7 @@ sdmmc_card_t* sdcard_init() {
     const char mount_point[] = MOUNT_POINT;
     ESP_LOGI(TAG, "Initializing SD card");
 
+#if defined(USE_SPI_MODE)
     // Use settings defined above to initialize SD card and mount FAT filesystem.
     // Note: esp_vfs_fat_sdmmc/sdspi_mount is all-in-one convenience functions.
     // Please check its source code and implement error recovery when developing
@@ -78,6 +82,41 @@ sdmmc_card_t* sdcard_init() {
     ESP_LOGI(TAG, "Mounting filesystem");
     ret = esp_vfs_fat_sdspi_mount(mount_point, &host, &slot_config, &mount_config, &card);
 
+#elif defined(USE_SDIO_MODE)
+    // Use settings defined above to initialize SD card and mount FAT filesystem.
+    // Note: esp_vfs_fat_sdmmc/sdspi_mount is all-in-one convenience functions.
+    // Please check its source code and implement error recovery when developing
+    // production applications.
+
+    ESP_LOGI(TAG, "Using SDMMC peripheral");
+
+    // By default, SD card frequency is initialized to SDMMC_FREQ_DEFAULT (20MHz)
+    // For setting a specific frequency, use host.max_freq_khz (range 400kHz - 40MHz for SDMMC)
+    // Example: for fixed frequency of 10MHz, use host.max_freq_khz = 10000;
+    sdmmc_host_t host = SDMMC_HOST_DEFAULT();
+
+    // This initializes the slot without card detect (CD) and write protect (WP) signals.
+    // Modify slot_config.gpio_cd and slot_config.gpio_wp if your board has these signals.
+    sdmmc_slot_config_t slot_config = SDMMC_SLOT_CONFIG_DEFAULT();
+
+    // Set bus width to use:
+    slot_config.width = 1;
+
+    // On chips where the GPIOs used for SD card can be configured, set them in
+    // the slot_config structure:
+    slot_config.clk = PIN_SDIO_CLK;
+    slot_config.cmd = PIN_SDIO_CMD;
+    slot_config.d0 = PIN_SDIO_D0;
+
+    // Enable internal pullups on enabled pins. The internal pullups
+    // are insufficient however, please make sure 10k external pullups are
+    // connected on the bus. This is for debug / example purpose only.
+    slot_config.flags |= SDMMC_SLOT_FLAG_INTERNAL_PULLUP;
+
+    ESP_LOGI(TAG, "Mounting filesystem");
+    ret = esp_vfs_fat_sdmmc_mount(mount_point, &host, &slot_config, &mount_config, &card);
+#endif
+
     if (ret != ESP_OK) {
         if (ret == ESP_FAIL) {
             ESP_LOGE(TAG, "Failed to mount filesystem. "
@@ -101,8 +140,4 @@ void sdcard_deinit(sdmmc_card_t* card) {
     // All done, unmount partition and disable SPI peripheral
     esp_vfs_fat_sdcard_unmount(MOUNT_POINT, card);
     ESP_LOGI(TAG, "Card unmounted");
-
-    //deinitialize the bus after all devices are removed
-    sdmmc_host_t host = SDSPI_HOST_DEFAULT();
-    spi_bus_free(host.slot);
 }
--- main/sdcard.h
+++ main/sdcard.h
@@ -2,12 +2,20 @@
 
 #define MOUNT_POINT "/sdcard"
 
+// Choose SD mode: SDIO or SPI
+// #define USE_SPI_MODE 1
+#define USE_SDIO_MODE 1
+
 // Pin assignments can be set in menuconfig, see "SD SPI Example Configuration" menu.
 // You can also change the pin assignments here by changing the following 4 lines.
 #define PIN_NUM_MISO  19
 #define PIN_NUM_MOSI  23
 #define PIN_NUM_CLK   18
 #define PIN_NUM_CS    5
+
+#define PIN_SDIO_CLK 39
+#define PIN_SDIO_CMD 38
+#define PIN_SDIO_D0  40
 
 sdmmc_card_t* sdcard_init();
 void sdcard_deinit(sdmmc_card_t* card);
