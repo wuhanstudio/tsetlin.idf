1859a078ee006a3d593b23af1d32373c49f6493e
wuhanstudio <wuhanstudios@gmail.com>
2026-01-22 16:17:34 +0000

    [platform][zephyr] Add list dir example

--- platforms/zephyr/src/main.c
+++ platforms/zephyr/src/main.c
@@ -1,6 +1,141 @@
-#include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/storage/disk_access.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/fs/fs.h>
 
-int main(int argc, char* argv[]) {
-	printf("Hello World");
+LOG_MODULE_REGISTER(main);
+
+#if defined(CONFIG_FAT_FILESYSTEM_ELM)
+	#define FS_RET_OK FR_OK
+#else
+	#define FS_RET_OK 0
+#endif
+
+#include <ff.h>
+
+#define DISK_DRIVE_NAME "SD"
+#define DISK_MOUNT_PT "/"DISK_DRIVE_NAME":"
+
+/* mounting info */
+static FATFS fat_fs;
+static struct fs_mount_t mp = {
+	.type = FS_FATFS,
+	.fs_data = &fat_fs,
+};
+static const char *disk_mount_pt = DISK_MOUNT_PT;
+
+static int lsdir(const char *path)
+{
+	int res;
+	struct fs_dir_t dirp;
+	static struct fs_dirent entry;
+	int count = 0;
+
+	fs_dir_t_init(&dirp);
+
+	/* Verify fs_opendir() */
+	res = fs_opendir(&dirp, path);
+	if (res) {
+		printk("Error opening dir %s [%d]\n", path, res);
+		return res;
+	}
+
+	printk("\nListing dir %s ...\n", path);
+	for (;;) {
+		/* Verify fs_readdir() */
+		res = fs_readdir(&dirp, &entry);
+
+		/* entry.name[0] == 0 means end-of-dir */
+		if (res || entry.name[0] == 0) {
+			break;
+		}
+
+		if (entry.type == FS_DIR_ENTRY_DIR) {
+			printk("[DIR ] %s\n", entry.name);
+		} else {
+			printk("[FILE] %s (size = %zu)\n",
+				entry.name, entry.size);
+		}
+		count++;
+	}
+
+	/* Verify fs_closedir() */
+	fs_closedir(&dirp);
+	if (res == 0) {
+		res = count;
+	}
+
+	return res;
+}
+
+int main(void)
+{
+	/* raw disk i/o */
+	do {
+		static const char *disk_pdrv = DISK_DRIVE_NAME;
+		uint64_t memory_size_mb;
+		uint32_t block_count;
+		uint32_t block_size;
+
+		if (disk_access_ioctl(disk_pdrv,
+				DISK_IOCTL_CTRL_INIT, NULL) != 0) {
+			LOG_ERR("Storage init ERROR!");
+			break;
+		}
+
+		if (disk_access_ioctl(disk_pdrv,
+				DISK_IOCTL_GET_SECTOR_COUNT, &block_count)) {
+			LOG_ERR("Unable to get sector count");
+			break;
+		}
+		LOG_INF("Block count %u", block_count);
+
+		if (disk_access_ioctl(disk_pdrv,
+				DISK_IOCTL_GET_SECTOR_SIZE, &block_size)) {
+			LOG_ERR("Unable to get sector size");
+			break;
+		}
+		printk("Sector size %u\n", block_size);
+
+		memory_size_mb = (uint64_t)block_count * block_size;
+		printk("Memory Size(MB) %u\n", (uint32_t)(memory_size_mb >> 20));
+
+		if (disk_access_ioctl(disk_pdrv,
+				DISK_IOCTL_CTRL_DEINIT, NULL) != 0) {
+			LOG_ERR("Storage deinit ERROR!");
+			break;
+		}
+	} while (0);
+
+	mp.mnt_point = disk_mount_pt;
+
+	int res = fs_mount(&mp);
+
+	if (res == FS_RET_OK) {
+		printk("Disk mounted.\n");
+		/* Try to unmount and remount the disk */
+		res = fs_unmount(&mp);
+		if (res != FS_RET_OK) {
+			printk("Error unmounting disk\n");
+			return res;
+		}
+		res = fs_mount(&mp);
+		if (res != FS_RET_OK) {
+			printk("Error remounting disk\n");
+			return res;
+		}
+		if (lsdir(disk_mount_pt) == 0) {
+			printk("Directory is empty.\n");
+		}
+	} else {
+		printk("Error mounting disk.\n");
+	}
+
+	fs_unmount(&mp);
+
+	while (1) {
+		k_sleep(K_MSEC(1000));
+	}
 	return 0;
 }
